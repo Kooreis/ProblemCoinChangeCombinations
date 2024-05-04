2fbad23cf1a679fd15a720d41683c43d50636551
Kooreis <kooreis.official@gmail.com>
2024-05-04 12:24:26 -0500

    This update introduces the 'coin_change' function. It initializes a dynamic programming array 'dp' with a size of 'amount + 1' and sets the first element to 1.

--- solution.py
+++ solution.py
@@ -1,23 +1,3 @@
-Sure, here is a Python console application that solves the coin change problem for combinations.
-
-```python
 def coin_change(coins, amount):
     dp = [0] * (amount + 1)
     dp[0] = 1
-
-    for coin in coins:
-        for x in range(coin, amount + 1):
-            dp[x] += dp[x - coin]
-
-    return dp[amount]
-
-def main():
-    coins = list(map(int, input("Enter the coin denominations (separated by spaces): ").split()))
-    amount = int(input("Enter the amount: "))
-    print("Number of combinations: ", coin_change(coins, amount))
-
-if __name__ == "__main__":
-    main()
-```
-
-In this application, you first enter the coin denominations separated by spaces, then enter the amount for which you want to find the number of combinations. The application will then print the number of combinations.
