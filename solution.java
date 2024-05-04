ffc940f3e44bdfda7078c193f46d88fc2946675b
Kooreis <kooreis.official@gmail.com>
2024-05-04 12:24:26 -0500

    In this update, the Scanner class is imported and the main method is defined. The user is prompted to enter the total amount, which is then stored in the 'amount' variable.

--- solution.java
+++ solution.java
@@ -1,6 +1,3 @@
-Here is a Java console application that solves the coin change problem for combinations:
-
-```java
 import java.util.Scanner;
 
 public class Main {
@@ -8,25 +5,5 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter the total amount: ");
         int amount = scanner.nextInt();
-        System.out.println("Enter the number of coin types: ");
-        int m = scanner.nextInt();
-        int[] coins = new int[m];
-        System.out.println("Enter the coin values: ");
-        for (int i = 0; i < m; i++) {
-            coins[i] = scanner.nextInt();
-        }
-        System.out.println("Total combinations: " + count(coins, m, amount));
-    }
-
-    static long count(int S[], int m, int n) {
-        long[] table = new long[n + 1];
-        table[0] = 1;
-        for (int i = 0; i < m; i++)
-            for (int j = S[i]; j <= n; j++)
-                table[j] += table[j - S[i]];
-        return table[n];
     }
 }
-```
-
-This program first takes the total amount and the number of coin types as input from the user. Then it takes the coin values as input. It then calculates the total number of combinations to make up the total amount using the given coin types and prints it.
