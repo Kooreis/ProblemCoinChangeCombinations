fd63e45280c3793ec4a2ae07fe35bcf0c232a6d0
Kooreis <kooreis.official@gmail.com>
2024-05-05 12:24:26 -0500

    Adding the Main method which is the entry point of the console application. It defines the coins and the amount for which we want to find combinations. It then calls the Change method and prints the result.

--- solution.cs
+++ solution.cs
@@ -1,6 +1,6 @@
-using System;
-using System.Collections.Generic;
-
-class Program
+static void Main(string[] args)
 {
+    int[] coins = { 1, 2, 5 };
+    int amount = 5;
+    Console.WriteLine(Change(amount, coins));
 }
