d85588b85bac8ef50152e87349c57b07114f584c
Kooreis <kooreis.official@gmail.com>
2024-05-05 12:24:26 -0500

    In this update, the user is prompted to enter the number of coin types, which is stored in the 'm' variable. An array 'coins' of size 'm' is created to store the coin values. The user is then prompted to enter the coin values, which are stored in the 'coins' array.

--- solution.java
+++ solution.java
@@ -1,9 +1,7 @@
-import java.util.Scanner;
-
-public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter the total amount: ");
-        int amount = scanner.nextInt();
-    }
-}
+System.out.println("Enter the number of coin types: ");
+        int m = scanner.nextInt();
+        int[] coins = new int[m];
+        System.out.println("Enter the coin values: ");
+        for (int i = 0; i < m; i++) {
+            coins[i] = scanner.nextInt();
+        }
